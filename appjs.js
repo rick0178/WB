47a07ca3ea7b1a1718d62572dfa339f9c1bd1e6f
Rick de Boer <rick.deboer@live.nl>
2017-12-11 11:31:25 +0100

    l

--- appjs.js
+++ appjs.js
@@ -4,34 +4,56 @@ var main = function() {
   var weekList = document.getElementById("weekly");
   var button = document.getElementById("add");
 
+  function habit(input) {
+    var name = document.createElement("span");
+    name.className = "habit";
+    name.appendChild(document.createTextNode(" " + input + " "));
+    return name;
+  }
+
+  function counter() {
+    var count = document.createElement("strong");
+    count.appendChild(document.createTextNode(" " + 0 + " "));
+    count.className = "counter";
+    return count;
+  }
   function adder() {
     if ($("#input").val() !== "") {
       var input = document.getElementById("input").value;
-      habit(input);
+      habitAdder(input);
       $("#input").val("");
     }
   }
 
-  function habit(input) {
-    var entry = document.createElement("li");
-    var count = 0;
+  function minButton() {
     var minButton = document.createElement("button");
     minButton.appendChild(document.createTextNode("-"));
     minButton.className = "minButton";
-    entry.appendChild(minButton);
-    entry.appendChild(document.createTextNode(" " + input + " "));
-    var counter = document.createElement("strong");
-    counter.appendChild(document.createTextNode(" " + count + " "));
-    counter.className = "counter";
-    entry.appendChild(counter);
+    return minButton;
+  }
+
+  function plusButton() {
     var plusButton = document.createElement("button");
     plusButton.appendChild(document.createTextNode("+"));
     plusButton.className = "plusButton";
-    entry.appendChild(plusButton);
+    return plusButton;
+  }
+
+  function checkbox() {
     var checkbox = document.createElement("input");
     checkbox.type = "checkbox";
     checkbox.className = "checkbox";
-    entry.appendChild(checkbox);
+    return checkbox;
+  }
+
+  function habitAdder(input) {
+    var entry = document.createElement("li");
+
+    entry.appendChild(new minButton());
+    entry.appendChild(new habit(input));
+    entry.appendChild(new counter());
+    entry.appendChild(new plusButton());
+    entry.appendChild(new checkbox());
     var amount = document.getElementById("howOften").value;
     if (amount == "Hourly") {
       hourList.appendChild(entry);
@@ -80,15 +102,16 @@ var main = function() {
   function updater() {
     var temp;
     var times = ["hourly", "daily", "weekly"];
-    for (x = 0; x < times.length; x++) {
-      if (times[x] === "hourly") {
-        var counts = document.querySelectorAll("#hourly .counter");
-      } else if (times[x] === "daily") {
-        var counts = document.querySelectorAll("#daily .counter");
-      } else {
-        var counts = document.querySelectorAll("#weekly .counter");
-      }
-      for (y = 0; y < counts.length; y++) {
+    for (y = 0; y < 100; y++) {
+      for (x = 0; x < times.length; x++) {
+        if (times[x] === "hourly") {
+          var counts = document.querySelectorAll("#hourly .counter");
+        } else if (times[x] === "daily") {
+          var counts = document.querySelectorAll("#daily .counter");
+        } else {
+          var counts = document.querySelectorAll("#weekly .counter");
+        }
+
         for (i = 1; i < counts.length; i++) {
           if (
             parseInt(counts[i - 1].innerHTML) < parseInt(counts[i].innerHTML)
