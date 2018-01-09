3111f200e485b461f5e8fab12ae7c9bfb1132c2f
Rick de Boer <rick.deboer@live.nl>
2018-01-09 21:26:03 +0100

    hihaho

--- express/app.js
+++ express/app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static(__dirname + "/Client"));
+app.use(express.static(__dirname + "/public"));
 app.use('/', page);
 
 // catch 404 and forward to error handler
